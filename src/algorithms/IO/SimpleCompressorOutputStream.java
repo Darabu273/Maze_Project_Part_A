32f713c02c15dc94b5cd43d34b8d6446ba35bc32
Darabu273 <dAr8194765>
2021-04-28 10:06:04 +0300

    write function SimpleCompressorOutputStream

--- src/algorithms/IO/SimpleCompressorOutputStream.java
+++ src/algorithms/IO/SimpleCompressorOutputStream.java
@@ -1,14 +1,12 @@
 package algorithms.IO;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class SimpleCompressorOutputStream extends OutputStream {
     OutputStream out;
 
-    public SimpleCompressorOutputStream(OutputStream out) {
-        this.out = out;
+    public SimpleCompressorOutputStream(OutputStream fileOutputStream) {
+        this.out = fileOutputStream;
     }
 
     @Override
@@ -22,9 +20,15 @@ public class SimpleCompressorOutputStream extends OutputStream {
         byte[] bytes = new  byte[b.length];
         int i;
         for (i = 0; i < 24; i++) { //copy the values of - rows, columns, start position and goal position as is to new byte array.
-            bytes[i] = b[i];
+            bytes[i] =(b[i]);
         }
-//        out.write(bytes);
+        out.write(bytes,0,24);
+        out.flush();
+        /*BufferedReader br = new BufferedReader(new FileReader("savedMaze.maze"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }*/
         int counter=0;
         int prev = 0; // prev position = we will start with zero
         for (int j = 24; j <b.length ; j++) { //will do different manipulate of write
@@ -32,26 +36,29 @@ public class SimpleCompressorOutputStream extends OutputStream {
                 counter++;
             }
             else{
+                int target = counter;
                 prev = 1-prev;//will change to 1 if prev was 0 and the same action to the opposite option.
                 if (counter > 255) // when counter bigger than 255 we will separate the num of the current byte with zero between the values.
                 {
-                   int target = counter;
                    while (target>255){
                        bytes[i] = (byte)255;
+                       out.write(bytes,i,1); // write to out the counter for this byte
                        target= target-255;
                        i++;
                        bytes[i] = 0; //separate the values - because the maximum value can be 255.
+                       out.write(bytes,i,1);// write to out the counter for this byte
                        i++;
                    }
                     bytes[i] =(byte)target; //remainder
                 }else{ // if the counter less then 255 we can represent in one byte.
-                    bytes[i] =(byte)counter;}
+                    bytes[i] =(byte)counter;
+                    out.write(bytes,i,1);}// write to out the counter for this byte
                 i++;
                 counter = 0;
             }
 
         }
-        out.write(bytes);
+
     }
 
 
